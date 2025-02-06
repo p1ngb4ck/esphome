d26aa89ffaaa4dbedacb96719d1b2a148e158515
Oliver Kleinecke <okleinecke@web.de>
2025-02-06 16:14:51 +0100

    Update mcp4461.h

--- esphome/components/mcp4461/mcp4461.h
+++ esphome/components/mcp4461/mcp4461.h
@@ -93,7 +93,7 @@ class Mcp4461Component : public Component, public i2c::I2CDevice {
   uint8_t get_wiper_address_(uint8_t wiper);
   uint16_t read_wiper_level_(uint8_t wiper);
   void write_wiper_level_(uint8_t wiper, uint16_t value);
-  void mcp4461_write_(uint8_t addr, uint16_t data, bool nonvolatile = false);
+  bool mcp4461_write_(uint8_t addr, uint16_t data, bool nonvolatile = false);
   uint8_t calc_terminal_connector_byte_(Mcp4461TerminalIdx terminal_connector);
 
   WiperState reg_[8];
