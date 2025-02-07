34dac471c4bab3054c8af5e6a13e51f9c200414d
Oliver Kleinecke <okleinecke@web.de>
2025-02-07 17:00:04 +0100

    Update mcp4461.cpp

--- esphome/components/mcp4461/mcp4461.cpp
+++ esphome/components/mcp4461/mcp4461.cpp
@@ -27,7 +27,7 @@ void Mcp4461Component::begin_() {
   for (uint8_t i = 0; i < 8; i++) {
     if (this->reg_[i].initial_value.has_value()) {
       uint16_t initial_state;
-      initial_state = static_cast<uint16_t>(this->reg_[i].initial_value * 1000.0);
+      initial_state = static_cast<uint16_t>(this->reg_[i].initial_value * 1000.f);
       this->write_wiper_level_(i, initial_state);
     }
     if (this->reg_[i].enabled) {
