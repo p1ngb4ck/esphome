de79c90b72cb36d4b7f4392b1ca461b574f06824
Oliver Kleinecke <okleinecke@web.de>
2025-02-09 17:22:18 +0100

    Update mcp4461_output.h

--- esphome/components/mcp4461/output/mcp4461_output.h
+++ esphome/components/mcp4461/output/mcp4461_output.h
@@ -30,7 +30,7 @@ class Mcp4461Wiper : public output::FloatOutput, public Parented<Mcp4461Componen
     }
   }
   uint16_t read_state();
-  uint16_t update_state();
+  void update_state();
   void save_level();
   void enable_wiper();
   void disable_wiper();
