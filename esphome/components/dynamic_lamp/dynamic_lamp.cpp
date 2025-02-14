d9b2ba937b19965221d5274863214d8123c9cdfb
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-02-14 17:06:57 +0100

    fix: use uniform initialization for input_id in set_available_outputs method

--- esphome/components/dynamic_lamp/dynamic_lamp.cpp
+++ esphome/components/dynamic_lamp/dynamic_lamp.cpp
@@ -100,7 +100,7 @@ void DynamicLamp::set_available_outputs(std::string output_list) {
   }
   for ( std::string s : v )
   {
-    std::string input_id = this->trim_(s.c_str());
+    std::string input_id{ this->trim_(s.c_str()) };
     this->available_outputs_[counter] = LinkedOutput{true, input_id, counter, 0, 0, 1.0, false};
     counter++;
   }
