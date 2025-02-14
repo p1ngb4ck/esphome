07752ad6fcaf325936a9cb4f91cff92abc8ffdf2
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-02-14 14:36:53 +0100

    trim whitespace from output list strings in set_available_outputs()

--- esphome/components/dynamic_lamp/dynamic_lamp.cpp
+++ esphome/components/dynamic_lamp/dynamic_lamp.cpp
@@ -89,6 +89,8 @@ void DynamicLamp::set_available_outputs(std::string output_list) {
   }
   for ( std::string s : v )
   {
+    std::string id_string;
+    id_string = std::regex_replace(s, std::regex("^ +| +$|( ) +"), "$1");
     this->available_outputs_[counter] = s.c_str();
     counter++;
   }
