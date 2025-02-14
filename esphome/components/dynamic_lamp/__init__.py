aa1e2d3cfe5918f0cbf50baec73ae06e1c43cc47
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-02-14 20:04:41 +0100

    fix: update CONFIG_SCHEMA to ensure available_outputs is a list for validation

--- esphome/components/dynamic_lamp/__init__.py
+++ esphome/components/dynamic_lamp/__init__.py
@@ -10,7 +10,7 @@ CONF_SAVE_MODE = 'save_mode'
 CONF_AVAILABLE_OUTPUTS = 'available_outputs'
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(DynamicLamp),
-    cv.Required(CONF_AVAILABLE_OUTPUTS): cv.All([cv.use_id(output.FloatOutput)]),
+    cv.Required(CONF_AVAILABLE_OUTPUTS): cv.All(cv.ensure_list, [cv.use_id(output.FloatOutput)]),
     cv.Optional(CONF_SAVE_MODE, default=0): cv.int_range(0, 1),
 }).extend(cv.COMPONENT_SCHEMA)
 
