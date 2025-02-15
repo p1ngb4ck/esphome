afcb501b04ddf91587925be061d7cb2cab68fef3
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-02-15 11:19:26 +0100

    fix: update CONFIG_SCHEMA to use list syntax for available_outputs and improve output handling in to_code

--- esphome/components/dynamic_lamp/__init__.py
+++ esphome/components/dynamic_lamp/__init__.py
@@ -10,7 +10,7 @@ CONF_SAVE_MODE = 'save_mode'
 CONF_AVAILABLE_OUTPUTS = 'available_outputs'
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(DynamicLamp),
-    cv.Required(CONF_AVAILABLE_OUTPUTS): cv.All(cv.ensure_list(cv.use_id(output.FloatOutput))),
+    cv.Required(CONF_AVAILABLE_OUTPUTS): [cv.use_id(output.FloatOutput)],
     cv.Optional(CONF_SAVE_MODE, default=0): cv.int_range(0, 1),
 }).extend(cv.COMPONENT_SCHEMA)
 
@@ -18,6 +18,7 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    for outputPointer in config.get(CONF_AVAILABLE_OUTPUTS):
-        cg.add(var.add_available_output(outputPointer))
+    for outputPointer in config.get(config[CONF_AVAILABLE_OUTPUTS]):
+        output_ = yield cg.get_variable(outputPointer)
+        cg.add(var.add_available_output(output_))
     cg.add(var.set_save_mode(config[CONF_SAVE_MODE]))
