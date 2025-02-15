227cf817aece7c984c55844c6b741d88911901de
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-02-15 21:51:05 +0100

    refactor: update write_state method to use parent active_lamps for improved clarity

--- esphome/components/dynamic_lamp/output/dynamic_lamp_output.cpp
+++ esphome/components/dynamic_lamp/output/dynamic_lamp_output.cpp
@@ -4,7 +4,7 @@ namespace esphome {
 namespace dynamic_lamp {
 
 void DynamicLamp::write_state(float state) {
-  if (this->active_lamps_[this->lamp_].set_level(state))
+  if (this->parent_->active_lamps_[this->lamp_].set_level(state))
   {
     this->state_ = state;
   }
