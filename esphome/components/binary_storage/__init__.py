f1e363b2fc13b389cea0c0fa9fde54f651a5b68f
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-11-15 23:18:30 +0000

    binary_storage changes

--- esphome/components/binary_storage/__init__.py
+++ esphome/components/binary_storage/__init__.py
@@ -358,8 +358,13 @@ async def to_code(config):
             # Auto-generate mount path from device id
             mount_path = f"/{config[CONF_ID]}"
 
-        # Create LittleFSMount component
-        mount_var = cg.new_Pvariable(LittleFSMount)
+        # Create LittleFSMount component with dynamically generated ID
+        from esphome.core import ID
+
+        mount_id = ID(
+            f"{config[CONF_ID]}_mount", is_declaration=True, type=LittleFSMount
+        )
+        mount_var = cg.new_Pvariable(mount_id)
         await cg.register_component(mount_var, config)
 
         cg.add(mount_var.set_storage_device(var))
