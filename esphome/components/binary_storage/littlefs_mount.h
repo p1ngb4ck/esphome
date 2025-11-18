d0d43ee42ab58d1628fd609e3e34b28c904b88cb
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-11-18 03:23:16 +0000

    changes to binary_storage

--- esphome/components/binary_storage/littlefs_mount.h
+++ esphome/components/binary_storage/littlefs_mount.h
@@ -8,27 +8,6 @@
 namespace esphome {
 namespace binary_storage {
 
-class BlockDeviceConfig {
- public:
-  uint32_t block_size;
-  uint32_t block_count;
-  uint32_t read_size;
-  uint32_t prog_size;
-  uint32_t lookahead_size;
-};
-
-class BinaryStorage {
- public:
-  virtual ~BinaryStorage() = default;
-  virtual int block_read(uint32_t lba, void *buffer, uint32_t size) = 0;
-  virtual int block_prog(const void *buffer, uint32_t lba, uint32_t size) = 0;
-  virtual int block_erase(uint32_t lba) = 0;
-  virtual int block_sync() = 0;
-  virtual BlockDeviceConfig get_block_config() const = 0;
-  virtual std::string get_device_name() const = 0;
-  virtual std::string get_device_type() const = 0;
-};
-
 class LittleFSMount : public Component {
  public:
   void set_storage(BinaryStorage *storage) { storage_ = storage; }
