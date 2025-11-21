6f8e50c1ef2d3fce4472b5d30f8a691571d55570
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-11-21 13:43:46 +0000

    storage-architecture-design-additions / changes

--- esphome/components/http_file_browser/http_file_browser.cpp
+++ esphome/components/http_file_browser/http_file_browser.cpp
@@ -1037,12 +1037,13 @@ bool HttpFileBrowser::delete_path(const std::string &path, bool is_directory) {
   // Check if this path is on network storage
   if (this->storage_ != nullptr) {
     auto *net_storage = this->storage_->find_network_storage_for_path(path);
+    std::string relative_path = this->storage_->strip_network_mount_prefix(net_storage, path);
     if (net_storage != nullptr) {
       // Use NetworkStorage API
       if (is_directory) {
-        return net_storage->delete_directory(path);
+        return net_storage->delete_directory(relative_path);
       } else {
-        return net_storage->delete_file(path);
+        return net_storage->delete_file(relative_path);
       }
     }
   }
@@ -1059,9 +1060,10 @@ bool HttpFileBrowser::create_dir(const std::string &path) {
   // Check if this path is on network storage
   if (this->storage_ != nullptr) {
     auto *net_storage = this->storage_->find_network_storage_for_path(path);
+    std::string relative_path = this->storage_->strip_network_mount_prefix(net_storage, path);
     if (net_storage != nullptr) {
       // Use NetworkStorage API
-      return net_storage->create_directory(path);
+      return net_storage->create_directory(relative_path);
     }
   }
 
