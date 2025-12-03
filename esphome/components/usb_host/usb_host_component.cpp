5f146b71f947f91c59dbe323260fce1118859d77
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-12-03 21:31:49 +0000

    storage-architecture-design-additions / changes

--- esphome/components/usb_host/usb_host_component.cpp
+++ esphome/components/usb_host/usb_host_component.cpp
@@ -73,6 +73,8 @@ void USBHost::setup() {
 
   // NEW: Register coordinator client for interface-class handler dispatch
   // This client receives device events and coordinates with VID/PID clients
+  // TEMPORARILY DISABLED TO DEBUG INTERRUPT STORM
+  /*
   usb_host_client_config_t client_config{
       .is_synchronous = false,
       .max_num_event_msg = 5,
@@ -92,6 +94,8 @@ void USBHost::setup() {
   } else {
     ESP_LOGD(TAG, "Coordinator client registered for interface-class handlers");
   }
+  */
+  ESP_LOGW(TAG, "Coordinator client registration DISABLED for debugging");
 
   // Mark USB Host as fully initialized
   this->initialized_ = true;
