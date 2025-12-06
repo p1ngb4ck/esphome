79de2c21297fa516aa40a4e575b15f526939394c
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-12-06 19:25:33 +0000

    storage-architecture-design-additions - continue dual usb host controller support dev

--- esphome/components/usb_host/usb_host_client.cpp
+++ esphome/components/usb_host/usb_host_client.cpp
@@ -234,7 +234,9 @@ void USBClient::setup() {
                                   .max_num_event_msg = 5,
                                   .async = {.client_event_callback = client_event_cb, .callback_arg = this}};
 
+  ESP_LOGE(TAG, "Calling usb_host_client_register_controller with host_handle=%p", this->parent_->get_host_handle());
   auto err = usb_host_client_register_controller(this->parent_->get_host_handle(), &config, &this->handle_);
+  ESP_LOGE(TAG, "usb_host_client_register_controller returned: %s", esp_err_to_name(err));
   if (err != ESP_OK) {
     ESP_LOGE(TAG, "client register failed: %s", esp_err_to_name(err));
     this->status_set_error(static_cast<const LogString *>(LOG_STR("Client register failed")));
