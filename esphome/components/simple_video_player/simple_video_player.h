770e0b3aa9cebb5de68d7a53633ede528da4cceb
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-12-07 22:15:51 +0000

    add initial simple_video_player_skeleton

--- esphome/components/simple_video_player/simple_video_player.h
+++ esphome/components/simple_video_player/simple_video_player.h
@@ -19,6 +19,19 @@ namespace esphome::simple_video_player {
 // Forward declarations
 class SimpleVideoPlayer;
 
+typedef struct {
+  void *sub_src; /*!< Sub source to keep media source extra data */
+} media_src_t;
+
+int media_src_storage_open(media_src_t *src);
+int media_src_storage_connect(media_src_t *src, const char *uri);
+int media_src_storage_disconnect(media_src_t *src);
+int media_src_storage_read(media_src_t *src, void *data, size_t len);
+int media_src_storage_seek(media_src_t *src, uint64_t position);
+int media_src_storage_get_position(media_src_t *src, uint64_t *position);
+int media_src_storage_get_size(media_src_t *src, uint64_t *size);
+int media_src_storage_close(media_src_t *src);
+
 /**
  * @brief Player states
  */
