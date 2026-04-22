6f5b639e0a975c05668c48df986fc09f159262c5
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2026-04-22 20:25:05 +0000

    latest config changes & ch934x debugger fix

--- esphome/components/uart/uart_debugger.h
+++ esphome/components/uart/uart_debugger.h
@@ -52,7 +52,7 @@ class UARTDebugger : public Component, public Trigger<UARTDirection, std::vector
     if (!debug_add_settings)
       return StringRef(debug_prefix.c_str());
     char buf[12];
-    StringRef res = "|";
+    std::string res = "|";
     snprintf(buf, sizeof(buf), "%" PRIu32, baud_rate);
     res += buf;
     res += ":";
