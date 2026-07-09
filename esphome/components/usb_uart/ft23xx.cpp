0fba85d91f2d78aab66a3ffa59ffca191bde98ae
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2026-07-09 19:35:58 +0200

    fix redefinition of on_rx_overflow

--- esphome/components/usb_uart/ft23xx.cpp
+++ esphome/components/usb_uart/ft23xx.cpp
@@ -339,11 +339,6 @@ void USBUartTypeFT23XX::on_rx_overflow(USBUartChannelBase *channel) {
   channel->input_buffer_.clear();
 }
 
-void USBUartTypeFT23XX::on_rx_overflow(USBUartChannelBase *channel) {
-  ESP_LOGW(TAG, "RX buffer overflow on channel %d, clearing to resync", channel->index_);
-  channel->input_buffer_.clear();
-}
-
 void USBUartTypeFT23XX::enable_channels() {
   if (!this->channels_.empty() && this->channels_[0]->initialised_.load()) {
     this->reset_(this->channels_[0]);
