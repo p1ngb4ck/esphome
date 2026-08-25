a2d1d56ac3952dcf3d0a730fe8f3ab934c038c5c
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2026-08-25 04:28:53 +0200

    re-vendor two tiny-fixes in safe_mode and time from testing_dev

--- esphome/components/safe_mode/safe_mode.cpp
+++ esphome/components/safe_mode/safe_mode.cpp
@@ -255,8 +255,10 @@ bool SafeModeComponent::should_enter_safe_mode(uint8_t num_attempts, uint32_t en
 }
 
 void SafeModeComponent::write_rtc_(uint32_t val) {
-  this->rtc_.save(&val);
-  global_preferences->sync();
+  bool ok = this->rtc_.save(&val);
+  ok = global_preferences->sync() && ok;
+  if (!ok)
+    ESP_LOGE(TAG, "Failed to persist boot-loop counter (%" PRIu32 "); bootloop protection may not work", val);
 }
 
 uint32_t SafeModeComponent::read_rtc_() {
@@ -272,7 +274,8 @@ void SafeModeComponent::clean_rtc() {
   // before sync, the boot wasn't really successful anyway and the counter should
   // remain incremented.
   uint32_t val = 0;
-  this->rtc_.save(&val);
+  if (!this->rtc_.save(&val))
+    ESP_LOGE(TAG, "Failed to reset boot-loop counter; bootloop protection may not work");
 }
 
 void SafeModeComponent::on_safe_shutdown() {
