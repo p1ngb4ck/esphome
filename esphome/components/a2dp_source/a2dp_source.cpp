ea3ad6f6684e57767822e9841e695446246455a5
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2026-09-01 13:47:59 +0200

    fix validation

--- esphome/components/a2dp_source/a2dp_source.cpp
+++ esphome/components/a2dp_source/a2dp_source.cpp
@@ -305,11 +305,11 @@ void A2DPSource::loop() {
   if (this->devices_changed_.exchange(false)) {
     this->publish_device_list_();
   }
-  uint8_t conn_state = this->pending_connection_state_.exchange(0xFF);
+  conn_state = this->pending_connection_state_.exchange(0xFF);
   if (conn_state != 0xFF) {
     ESP_LOGD(TAG, "Connection state: %s", this->source_.to_str((esp_a2d_connection_state_t) conn_state));
   }
-  uint8_t audio_state = this->pending_audio_state_.exchange(0xFF);
+  audio_state = this->pending_audio_state_.exchange(0xFF);
   if (audio_state != 0xFF) {
     ESP_LOGD(TAG, "Audio state: %s", this->source_.to_str((esp_a2d_audio_state_t) audio_state));
   }
